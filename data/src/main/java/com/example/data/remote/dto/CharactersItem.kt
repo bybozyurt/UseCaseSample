ef4bf052222e7c0e7eace7776d780d2fd478740f
= <=>
2021-11-30 23:56:31 +0300

    roomDB added and working

--- data/src/main/java/com/example/data/remote/dto/CharactersItem.kt
+++ data/src/main/java/com/example/data/remote/dto/CharactersItem.kt
@@ -1,40 +1,59 @@
 package com.example.data.remote.dto
 
 
+import android.text.BoringLayout
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.data.domain.model.Characters
+import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 /**
  * Created by Ahmet Bozyurt on 22.11.2021
  */
+//@Entity
 data class CharactersItem (
 
+//    @PrimaryKey
+//    @ColumnInfo(name = "id")
     @SerializedName("id")
     val id: Int,
 
+    //@ColumnInfo(name = "firstName")
     @SerializedName("firstName")
     val firstName: String,
 
+    //@ColumnInfo(name = "lastName")
     @SerializedName("lastName")
     val lastName: String,
 
+    //@ColumnInfo(name = "fullName")
     @SerializedName("fullName")
     val fullName: String,
 
+    //@ColumnInfo(name = "title")
     @SerializedName("title")
     val title: String,
 
+    //@ColumnInfo(name = "family")
     @SerializedName("family")
     val family: String,
 
+    //@ColumnInfo(name = "image")
     @SerializedName("image")
     val image: String,
 
-    @SerializedName("imageUrl")
+   // @ColumnInfo(name = "imageUrl")
+    //@SerializedName("imageUrl")
     val imageUrl: String,
 
-
+    //@ColumnInfo(name = "randomNumber")
     var randomNumber : Int,
+
+    //@ColumnInfo(name = "flag")
+    @Expose
+    var flag : Boolean = false,
 
     )
 
@@ -47,7 +66,8 @@ fun CharactersItem.toCharacters() : Characters {
         lastName = lastName,
         title = title,
         imageUrl = imageUrl,
-        randomNumber = randomNumber
+        randomNumber = randomNumber,
+        flag = flag
     )
 }
 
--- presentation/src/main/java/com/example/presentation/character_list/CharacterListState.kt
+++ presentation/src/main/java/com/example/presentation/character_list/CharacterListState.kt
@@ -1,6 +1,7 @@
 package com.example.presentation.character_list
 
 import com.example.data.domain.model.Characters
+import com.example.data.remote.dto.CharactersItem
 
 /**
  * Created by Ahmet Bozyurt on 22.11.2021
@@ -8,7 +9,8 @@ import com.example.data.domain.model.Characters
 data class CharacterListState (
     val isLoading : Boolean = false,
     val characters : List<Characters> = emptyList(),
-    val error : String = ""
+    val error : String = "",
+    val dataCharacters : List<CharactersItem> = emptyList()
 )
 
 
